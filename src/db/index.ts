b5279040a148d9a701a8f4327cf1d332e0232f53
pbentivengo <pablo.bentivengo@abstract.it>
2024-12-18 15:40:33 +0100

    remove errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-import schemas from "./schema";
+
 
 export const db = drizzle(neon(process.env.POSTGRES_URL!), {
-  schema: { ...schemas },
+
 });
--- src/db/schemas/subscriptions.ts
+++ src/db/schemas/subscriptions.ts
@@ -1,13 +1,10 @@
 
-import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, uuid } from "drizzle-orm/pg-core";
 
 
 
 export const countryTable = pgTable("subcriptions", {
     id: uuid("id").primaryKey(),
-    name: text("name").notNull(),
-    mail: text("mail").notNull(),
-    phone: text("phone").notNull(),
-    cuit: text("cuit").notNull().unique(),
+
 });
   
