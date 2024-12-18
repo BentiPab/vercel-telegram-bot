d5fd70b9c5afea96ee462542fa8fd604827d19f2
pbentivengo <pablo.bentivengo@abstract.it>
2024-12-18 16:06:27 +0100

    add cors

--- src/api/telegram/routes.ts
+++ src/api/telegram/routes.ts
@@ -1,6 +1,41 @@
 import bot from '@/bot';
+import Cors from 'cors';
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const cors = Cors({
+    methods: ['POST'],
+    origin: (origin, callback) => {
+      // Restrict to Telegram's servers (you can adjust this if needed)
+      if (!origin || origin === 'https://api.telegram.org') {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('CORS policy: Only Telegram servers allowed'), false); // Deny the request
+      }
+    },
+  });
 
-export default async function handler(req: Request) {
+// Helper function to run the middleware
+async function runCors(req: Request, res: Response) {
+    return await new Promise<void>((resolve, reject) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        cors(req as any, res as any, (result) => {
+          if (result instanceof Error) {
+            return reject(result); // Reject the promise if there's an error
+          }
+          resolve(); // Resolve the promise when CORS is successfully handled
+        });
+      });
+}
+
+export default async function handler(req: Request,res:Response) {
+    await runCors(req, res);
+    const telegramSecretToken = req.headers.get('x-telegram-bot-api-secret-token');
+    if (telegramSecretToken !== TELEGRAM_BOT_TOKEN) {
+        return new Response(JSON.stringify({ message: 'Forbidden' }), {
+            status: 403,
+            headers: { 'Content-Type': 'application/json' },
+          });
+    }
+
   if (req.method === 'POST') {
     try {
       const body = await req.json(); 
